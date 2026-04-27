ab3edd1c70961e6f46187daee6239a25c64470bb
mo1998 <mo.181298@gmail.com>
2026-04-27 09:52:22 +0300

    add training script for CIFAR100 dataset with data preparation and hyperparameters

---
+++ train.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from models import ResNet18
+
+# 1. Hardware & Hyperparameters
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = 32 # Keep small for 8GB RAM
+lr = 0.1
+
+print(f"Using device: {device}")
+
+# 2. Data Preparation
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+])
+
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
